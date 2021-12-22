0ffad52b86a73f58dec9db7abf3f836559c92ec9
Osama Asif <osama.asif20@gmail.com>
2021-12-22 20:34:07 +0500

    * Android configuration completed

--- android/app/src/main/java/com/todoapp/MainApplication.java
+++ android/app/src/main/java/com/todoapp/MainApplication.java
@@ -9,7 +9,9 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
+import com.facebook.react.bridge.JSIModulePackage;
 import java.util.List;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -32,6 +34,10 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+          return new ReanimatedJSIModulePackage(); // <- add
         }
       };
 
